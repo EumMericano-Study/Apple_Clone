25ca070c0b8b8b77f23b7a44fa42fc4cde0da96b
EumYoungHyun <opwer032@naver.com>
2021-06-12 15:29:02 +0900

    feat: Add load logic

--- js/main_eum.js
+++ js/main_eum.js
@@ -1,11 +1,11 @@
 (() => {
-  let pageYOffset = 0;
+  let yOffset = 0;
   let prevScrollHeight = 0;
   let currentScene = 0;
 
   const sceneInfo = [
     {
-      // 0
+      index: 0,
       type: "sticky",
       heightNum: 5, // 브라우저 높이의 N배수로 scrollHeight 설정
       scrollHeight: 0,
@@ -14,7 +14,7 @@
       },
     },
     {
-      // 1
+      index: 1,
       type: "normal",
       heightNum: 5,
       scrollHeight: 0,
@@ -23,7 +23,7 @@
       },
     },
     {
-      // 2
+      index: 2,
       type: "sticky",
       heightNum: 5,
       scrollHeight: 0,
@@ -32,7 +32,7 @@
       },
     },
     {
-      // 3
+      index: 3,
       type: "sticky",
       heightNum: 5,
       scrollHeight: 0,
@@ -44,13 +44,24 @@
 
   function setLayout() {
     // 각 스크롤 섹션의 높이 세팅
-
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
   function scrollLoop() {
@@ -58,20 +69,22 @@
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
-    if (pageYOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       if (currentScene === sceneInfo.length - 1) return;
       currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
     if (pageYOffset < prevScrollHeight) {
       if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
   }
 
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
-    pageYOffset = window.pageYOffset;
+    yOffset = window.pageYOffset;
     scrollLoop();
   });
-  setLayout();
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })();
