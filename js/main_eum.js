af1e55cf914fc7568a6f1958b7336221a38b788c
EumYoungHyun <opwer032@naver.com>
2021-07-08 04:01:09 +0900

    feat: Calc image blend using sx, sy

--- js/main_eum.js
+++ js/main_eum.js
@@ -195,7 +195,7 @@
       values: {
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
-        imageBlendY: [0, 0, { start: 0, end: 0 }],
+        blendHeight: [0, 0, { start: 0, end: 0 }],
         rectStartY: 0,
       },
     },
@@ -614,7 +614,24 @@
         } else {
           // 첫 canvas가 꽉 찬 후
           step = 2;
-          objs.context.drawImage(objs.images[1], 0, 200);
+          values.blendHeight[0] = 0;
+          values.blendHeight[1] = objs.canvas.height;
+          values.blendHeight[2].start = values.rect1X[2].end + 0.05;
+          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+          objs.context.drawImage(
+            objs.images[1],
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight,
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight
+          );
 
           objs.canvas.classList.add("sticky");
           objs.canvas.style.top = `${
